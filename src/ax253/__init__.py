9136c2a0fe03ca2849b40a32a2bea268b25f6580
Masen Furer <m_github@0x26.net>
2022-05-28 14:33:42 -0700

    tnc2: add tnc2 decode protocol (for APRS-IS, etc)

--- src/ax253/__init__.py
+++ src/ax253/__init__.py
@@ -3,6 +3,7 @@ from importlib_metadata import version
 from .address import Address
 from .decode import GenericDecoder, FrameDecodeProtocol, SyncFrameDecode
 from .frame import AX25BytestreamDecoder, Control, Frame, FrameType
+from .tnc2 import TNC2Protocol, TNC2Decode
 
 __author__ = "Masen Furer KF7HVM <kf7hvm@0x26.net>"
 __copyright__ = "Copyright 2022 Masen Furer and Contributors"
@@ -18,6 +19,8 @@ __all__ = [
     "FrameType",
     "GenericDecoder",
     "SyncFrameDecode",
+    "TNC2Decode",
+    "TNC2Protocol",
     "__distribution__",
     "__version__",
 ]
